83417232613494e80c4395d818abe7906d239fd9
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-24 11:42:18 -0300

    Desafio1

---
+++ src/main/java/stream_api/desafios/Desafio.java
@@ -0,0 +1,13 @@
+package stream_api.desafios;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+* Classe base para todos os desafios
+*/
+public class Desafio {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+    }
+}
