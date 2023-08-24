2ed478980a742153a795b4a39414aad3778cae7d
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-08-24 11:58:40 -0300

    Desafio2

--- src/main/java/stream_api/desafios/Desafio.java
+++ src/main/java/stream_api/desafios/Desafio.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
-* Classe base para todos os desafios
+    Classe base para todos os desafios
 */
 public class Desafio {
     public static void main(String[] args) {
